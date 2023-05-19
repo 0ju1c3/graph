9c2b0246a09f605fbf6087f726f2add0b3b6cb7d
Ojas Tapadia <114589028+0ju1c3@users.noreply.github.com>
2023-05-19 18:47:19 +0530

    Update animation.py

--- sammard/animation.py
+++ sammard/animation.py
@@ -1,10 +1,11 @@
 import pandas as pd
-from PIL import Image
+#from PIL import Image
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation, FFMpegWriter
 plt.style.use('ggplot')#print(plt.style.available); different built-in-styles
-from matplotlib.animation import FuncAnimation
+
 def animate(i):
-    data = pd.read_csv('flightSimulatorResults.csv')
+    data = pd.read_csv('flightSimulatorResults.csv')#data is being read and loaded into memory on each frame of the animation
     t = data['Time']
     v = data['Velocity']
     a = data['Acceleration']
@@ -26,7 +27,9 @@ def animate(i):
     plt.plot(t,alt)
     plt.xlabel('Time')
     plt.ylabel('Altitude')
-ani = FuncAnimation(plt.gcf(),animate,interval = 1000, frames = 500, repeat = False)
+ani = FuncAnimation(plt.gcf(),animate,interval = 1000, frames = 10, repeat = False)
+writer = FFMpegWriter(fps=20)
+#ani.save('flight_animation.mp4',writer=writer)
 plt.tight_layout()
 plt.show()
 exit()
